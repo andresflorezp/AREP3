7a010004c56961ccaee87faff955225a50b8aec9
andres97 <anflo.97@gmail.com>
2019-01-28 20:55:56 -0500

    DATAGRAMAS

--- ARSW-Lab2/src/main/java/edu/eci/arsw/DatagramTimeClient.java
+++ ARSW-Lab2/src/main/java/edu/eci/arsw/DatagramTimeClient.java
@@ -9,30 +9,43 @@ import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class DatagramTimeClient {
-
-	public static void main(String[] args) {
-
-		byte[] sendBuf = new byte[256];
-		try {
-			DatagramSocket socket = new DatagramSocket();
-			byte[] buf = new byte[256];
-			InetAddress address = InetAddress.getByName("127.0.0.1");
-			DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4098);
-			socket.send(packet);
-			packet = new DatagramPacket(buf, buf.length);
-			socket.receive(packet);
-			String received = new String(packet.getData(), 0, packet.getLength());
-			System.out.println("Date: " + received);
-
-		} catch (
-
-		SocketException ex) {
-			Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
-		} catch (UnknownHostException ex) {
-			Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
-		} catch (IOException ex) {
-			Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
+public class DatagramTimeClient implements Runnable {
+	DatagramSocket socket;
+	
+	public DatagramTimeClient() throws SocketException {
+		// TODO Auto-generated constructor stub
+		socket = new DatagramSocket();
+		
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for (;;) {
+			byte[] sendBuf = new byte[256];
+			try {
+				
+				byte[] buf = new byte[256];
+				InetAddress address = InetAddress.getByName("127.0.0.1");
+				DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4565);
+				socket.send(packet);
+				packet = new DatagramPacket(buf, buf.length);
+				socket.receive(packet);
+				String received = new String(packet.getData(), 0, packet.getLength());
+				System.out.println("Date: " + received);
+			} catch (SocketException ex) {
+				Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (UnknownHostException ex) {
+				Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (IOException ex) {
+				Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
+			}
 		}
+
+	}
+	public static void main(String[] args) throws SocketException {
+		DatagramTimeClient datagram= new DatagramTimeClient();
+		datagram.run();
+
 	}
 }
--- ARSW-Lab2/src/main/java/edu/eci/arsw/DatagramTimeServer.java
+++ ARSW-Lab2/src/main/java/edu/eci/arsw/DatagramTimeServer.java
@@ -9,43 +9,47 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class DatagramTimeServer {
+public class DatagramTimeServer implements Runnable {
 
 	DatagramSocket socket;
 
 	public DatagramTimeServer() {
 		try {
-			socket = new DatagramSocket(4098);
+			socket = new DatagramSocket(4565);
 		} catch (SocketException ex) {
 			Logger.getLogger(DatagramTimeServer.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 
-	public void startServer() throws InterruptedException {
-		byte[] buf = new byte[256];
-		
-		try {
-			DatagramPacket packet = new DatagramPacket(buf, buf.length);
-			socket.receive(packet);
-			String dString = new Date().toString();
-			buf = dString.getBytes();
-			InetAddress address = packet.getAddress();
-			int port = packet.getPort();
-			packet = new DatagramPacket(buf, buf.length, address, port);
-			socket.send(packet);
-			Thread.currentThread().wait(5000);
-			String[] L = {"1"};
-			DatagramTimeClient.main(L);
-
-		} catch (IOException ex) {
-			Logger.getLogger(DatagramTimeServer.class.getName()).log(Level.SEVERE, null, ex);
-		}
-
-		socket.close();
+	public static void main(String[] args) {
+		DatagramTimeServer ds = new DatagramTimeServer();
+		ds.run();
 	}
 
-	public static void main(String[] args) throws InterruptedException {
-		DatagramTimeServer ds = new DatagramTimeServer();
-		ds.startServer();
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for (;;) {
+			byte[] buf = new byte[256];
+			try {
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				socket.receive(packet);
+				String dString = new Date().toString();
+				buf = dString.getBytes();
+				InetAddress address = packet.getAddress();
+				int port = packet.getPort();
+				packet = new DatagramPacket(buf, buf.length, address, port);
+				socket.send(packet);
+				Thread.sleep(5000);
+			} catch (IOException ex) {
+				Logger.getLogger(DatagramTimeServer.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			//socket.close();
+			catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 	}
 }
